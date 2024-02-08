534c081b34232c0f7e7792f4c36571b102822527
Adam Damiano <adam@adamlearns.live>
2024-02-08 11:31:27 -0800

    Improve the script
    - It now has trigger phrases for returning from a break
    - Fixed a bug with reading music notes when I would hum
    - Made other minor improvements not worth listing

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import obspython as S
 import pathlib
 import subprocess
+import time
 
-trigger_phrases = ["is it jump time", "break break break", "brake brake brake"]
-path_to_captions_file = "/Users/adam/tmp/stream-captions.txt"
+start_break_trigger_phrases = ["is it jump time", "break break break", "brake brake brake"]
+stop_break_trigger_phrases = ["I'm back"]
+
+path_to_live_captions_file = "/Users/adam/tmp/stream-captions.txt"
+path_to_shadow_captions_file = "/Users/adam/tmp/stream-captions2.txt"
 
 # This is what we'll mute when the trigger phrase is uttered
 mic_name = "Mic/Aux"
 
+shadow_mic_name = "Test duplicate mic"
+
 # This is the scene we'll switch to when the trigger phrase is uttered
 scene_name_to_switch_to = "Stream game"
+
+# Handle to the game process
+game_proc = None
 
 def set_scene_by_name(scene_name):
   scenes = S.obs_frontend_get_scenes()
@@ -30,24 +39,34 @@ def truncate_file(path):
     with open(path, "w") as file:
       file.truncate()
 
-def was_trigger_phrase_uttered():
-  with open(path_to_captions_file, "r") as file:
+def was_trigger_phrase_uttered(phrases, path_to_captions_file):
+  with open(path_to_captions_file, "r", encoding="utf-8") as file:
     for line in file:
-      line = line.replace(",", "").replace(".", "")
-      for trigger_phrase in trigger_phrases:
+      line = line.replace(",", "").replace(".", "").lower()
+      for trigger_phrase in phrases:
         if trigger_phrase in line:
           print("Trigger phrase found: " + trigger_phrase)
-          # Delete the file so that we don't find the phrase again
+          # Truncate the file so that we don't find the phrase again
           truncate_file(path_to_captions_file)
           return True
 
   return False
 
-def check_for_trigger_phrase():
-  if was_trigger_phrase_uttered():
+def check_for_trigger_phrases():
+  global game_proc
+  if was_trigger_phrase_uttered(start_break_trigger_phrases, path_to_captions_file):
     trigger_phrase_uttered()
 
+  if game_proc is not None and was_return_from_break_trigger_phrase_uttered(stop_break_trigger_phrases, path_to_shadow_captions_file):
+    set_mic_muted(False)
+    set_scene_by_name("Regular streaming")
+
+    game_proc.kill()
+    game_proc = None
+
+# Returns True if this succeeded
 def set_mic_muted(muted):
+  print(f'{"Muting" if muted else "Unmuting"} the mic')
   mic = S.obs_get_source_by_name(mic_name)
   if mic is None:
     print(f"Couldn't find the mic by \"{mic_name}\" to be able to mute/unmute it")
@@ -55,26 +74,47 @@ def set_mic_muted(muted):
     S.obs_source_set_muted(mic, muted)
     S.obs_source_release(mic)
 
+  return mic is not None
+
 def trigger_phrase_uttered():
+  global game_proc
+  if not set_mic_muted(True):
+    # If the mic can't be muted, don't do anything else, that way there's an
+    # obvious indicator that it couldn't be muted.
+    return
+
   print("Launching Jump Royale")
-  subprocess.Popen(['/Applications/Godot_mono.app/Contents/MacOS/Godot', '--path', '/Volumes/inland/code/JumpRoyale/JumpRoyale'])
+  game_proc = subprocess.Popen(['/Applications/Godot_mono.app/Contents/MacOS/Godot', '--path', '/Volumes/inland/code/JumpRoyale/JumpRoyale'])
+
+  time.sleep(1.0)
+
+  # If the process couldn't start...
+  if game_proc.poll() is not None:
+    print("Godot already died. Unmuting the mic.")
+    set_mic_muted(False)
+    return
 
   print("Switching OBS scenes to the game")
   set_scene_by_name(scene_name_to_switch_to)
 
-  print("Muting the mic")
-  set_mic_muted(True)
-
 def script_unload():
-  S.timer_remove(check_for_trigger_phrase)
+  S.timer_remove(check_for_trigger_phrases)
+
+def make_strings_lowercase(strings):
+  for i in range(len(strings)):
+    strings[i] = strings[i].lower()
 
 def main():
   print("Script is running")
+  make_strings_lowercase(start_break_trigger_phrases)
+  make_strings_lowercase(stop_break_trigger_phrases)
 
   # Make sure we don't have any dangling trigger phrases
-  truncate_file(path_to_captions_file)
-  S.timer_add(check_for_trigger_phrase, 1000)
-  print(f"Timer started to check for phrases: {trigger_phrases}")
+  truncate_file(path_to_live_captions_file)
+  truncate_file(path_to_shadow_captions_file)
+
+  S.timer_add(check_for_trigger_phrases, 500)
+  print(f"Timer started to check for start-break phrases {start_break_trigger_phrases} and stop-break phrases {stop_break_trigger_phrases}")
 
 
 main()
